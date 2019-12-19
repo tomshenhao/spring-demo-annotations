e77f9725f6d45a38ae19b69103f8ebb3591f8bdf
t822660 <t822660@spark.co.nz>
2019-12-20 09:55:03 +1300

    update files

--- src/com/luv2code/springdemo/SportConfig.java
+++ src/com/luv2code/springdemo/SportConfig.java
@@ -2,8 +2,11 @@ package com.luv2code.springdemo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:sport.properties")
+
 public class SportConfig {
 	
 	//define bean for our sad fortune service
--- src/com/luv2code/springdemo/SwimCoach.java
+++ src/com/luv2code/springdemo/SwimCoach.java
@@ -1,8 +1,16 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class SwimCoach implements Coach {
 
 	private FortuneService fortuneService;
+	
+	@Value("${foo.email}")
+	private String email;
+	
+	@Value("${foo.team}")
+	private String team;
 	
 	public SwimCoach(FortuneService theFortuneService) {
 		fortuneService=theFortuneService;
@@ -15,6 +23,13 @@ public class SwimCoach implements Coach {
 	@Override
 	public String getDailyFortune() {
 		return fortuneService.getFortune();
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public String getTeam() {
+		return team;
 	}
 
 }
